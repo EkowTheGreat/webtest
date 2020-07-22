139208df59fef0826386f8aed03634ac9db80ab0
EkowTheGreat <56401507+EkowTheGreat@users.noreply.github.com>
2020-07-22 13:58:47 +0100

    twitch
    added twitch, youtube and editied look and template

--- etgSite/src/app/routing/routing.module.ts
+++ etgSite/src/app/routing/routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {  HomeComponent } from '../home/home.component';
 import {  StreamComponent } from '../stream/stream.component';
+import{ YoutubeComponent} from '../youtube/youtube.component';
 
 
 const routes = [
@@ -13,6 +14,10 @@ const routes = [
 
     path: 'stream',
     component: StreamComponent
+  },
+  {
+    path: 'youtube',
+    component: YoutubeComponent
   }
 ];
 
